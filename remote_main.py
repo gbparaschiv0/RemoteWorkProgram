5c9b2f695311d46fb44a30594ba34150221ff751
George Paraschiv <paraschiv.bogdan.87@gmail.com>
2020-03-21 20:34:03 +0100

    Implemented delay and button swap color on operations

--- remote_main.py
+++ remote_main.py
@@ -4,9 +4,24 @@ from remote_UI import Ui_mainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ctypes import*
 import time
+import configparser
 
 on = 1
 off = 0
+
+class iniReader:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read("remote.ini")
+    
+    def readSeconds(self):
+        return int(self.config["DEFAULT"]["Seconds"])
+
+    def writeSeconds(self, seconds: str):
+        if(seconds != self.config["DEFAULT"]["Seconds"]):
+            self.config["DEFAULT"]["Seconds"] = seconds
+            with open("remote.ini", "w") as configfile:
+                self.config.write(configfile)
 
 class usbControl:
     __ECU_pins = 12
@@ -41,19 +56,14 @@ class usbControl:
         else:
             self.__pinStatus &= ~self.__power
         self.__write()
-        return
 
     def flash_mode(self):
         self.__pinStatus &= ~self.__ECU_pins
-        self.power(off)
-        time.sleep(1)
-        self.power(on)
+        self.__write()
 
     def run_mode(self):
         self.__pinStatus |= self.__ECU_pins
-        self.power(off)
-        time.sleep(1)
-        self.power(on)
+        self.__write()
 
 class operationsType:
     def __init__(self, bt: button):
@@ -74,7 +84,7 @@ class operationsType:
         self.__bt.link.lButton.uiButton.setEnabled(True)
 
 class commands:
-    def __init__(self, bt: button):
+    def __init__(self, bt):
         self.bt = bt
         self.operation = operationsType(bt)
 
@@ -95,13 +105,21 @@ class disconnectCm(commands):
 
 class flashCm(commands):
     def execute(self):
+        self.bt.link.powerOffBt.cm.execute()
         MCP2200.flash_mode()
+        time.sleep(1)
+        self.bt.link.powerOnBt.cm.execute()
+
         self.operation.swapColors()
         self.operation.swapButtonActivation()
 
 class runCm(commands):
     def execute(self):
+        self.bt.link.powerOffBt.cm.execute()
         MCP2200.run_mode()
+        time.sleep(1)
+        self.bt.link.powerOnBt.cm.execute()
+
         self.operation.swapColors()
         self.operation.swapButtonActivation()
 
@@ -119,42 +137,69 @@ class offCm(commands):
 
 class restartPwCm(commands):
     def execute(self):
-        print("restartPwCm")
+        self.bt.link.powerOffBt.cm.execute()
+        time.sleep(self.bt.link.uiTextBox.seconds)
+        self.bt.link.powerOnBt.cm.execute()
 
 class fastRestartCm(commands):
     def execute(self):
-        MCP2200.power(off)
+        self.bt.link.powerOffBt.cm.execute()
         time.sleep(1)
-        MCP2200.power(on)
-
-class reloadCm(commands):
+        self.bt.link.powerOnBt.cm.execute()
+    
+class iniSaveCm(commands):
     def execute(self):
-        print("reloadCm")
+        try:
+            self.bt.seconds = int(self.bt.uiTextEdit.toPlainText())
+            self.bt.iniR.writeSeconds(str(self.bt.seconds))
+
+        except ValueError:
+            pass
 
 class linkType:
     __buttonLinked = False
     def __init__(self, bt: button):
         self.__bt = bt
 
+    """ Also link viseversa """
     def buttonLink(self, bt: button):
         if(self.__buttonLinked == False):
             self.__buttonLinked = True
             self.lButton = bt
             bt.link.buttonLink(self.__bt)
 
+    """ Link of a frame """
     def frameLink(self, frame: QtWidgets.QFrame):
         self.lFrame = frame
+
+    """ Link for powerbuttons """
+    def powerLink(self, powerOnBt: button, powerOffBt: button):
+        self.powerOnBt = powerOnBt
+        self.powerOffBt = powerOffBt
+
+    """ TextBox Link """
+    def textBoxLink(self, uiTextBox: editBox):
+        self.uiTextBox = uiTextBox
 
 class button:
     def __init__(self, uiButton: QtWidgets.QPushButton, cm: commands):
         self.uiButton = uiButton
         self.cm = cm(self)
-        uiButton.clicked.connect(self.cm.execute)
+        self.uiButton.clicked.connect(self.cm.execute)
         self.link = linkType(self)
+
+class editBox:
+    def __init__(self, uiTextEdit: QtWidgets.QTextEdit, cm: commands):
+        self.uiTextEdit = uiTextEdit
+        self.cm = cm(self)
+        self.iniR = iniReader()
+        self.seconds = self.iniR.readSeconds()
+        self.uiTextEdit.setPlainText(str(self.seconds))
+        self.uiTextEdit.textChanged.connect(self.cm.execute)
 
 class RemoteApp:
     def __init__(self, ui: Ui_mainWindow):
-        # Assigned Buttons
+        """ Assigned Buttons """
         self.ui = ui
         self.connectBt = button(ui.ConnectButton, connectCm)
         self.disconnectBt = button(ui.DisconnectButton, disconnectCm)
@@ -164,20 +209,29 @@ class RemoteApp:
         self.offBt = button(ui.offButton, offCm)
         self.restartPwBt = button(ui.RestartPowerButton, restartPwCm)
         self.fastRestartBt = button(ui.FastRestart, fastRestartCm)
-        self.reloadBt = button(ui.ReloadButton, reloadCm)
+        self.uiEditBox = editBox(ui.DelayValue, iniSaveCm)
 
-        # Link Buttons
+        """ Link Buttons """
         self.connectBt.link.buttonLink(self.disconnectBt)
         self.connectBt.link.frameLink(self.ui.connectedFrame)
         self.disconnectBt.link.frameLink(self.ui.disconnectFrame)
         self.flashBt.link.buttonLink(self.runBt)
         self.onBt.link.buttonLink(self.offBt)
 
-        # workaround bug
+        """ Power button link """
+        self.flashBt.link.powerLink(self.onBt, self.offBt)
+        self.runBt.link.powerLink(self.onBt, self.offBt)
+        self.restartPwBt.link.powerLink(self.onBt, self.offBt)
+        self.fastRestartBt.link.powerLink(self.onBt, self.offBt)
+
+        """ EditBox link """
+        self.restartPwBt.link.textBoxLink(self.uiEditBox)
+
+        """ workaround bug """
         self.ui.connectedFrame.setEnabled(False)
 
 if __name__ == "__main__":
-    # Init the UI
+    """ Init the UI """
     MCP2200 = usbControl()
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = QtWidgets.QMainWindow()
